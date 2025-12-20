33eac2bcc830df4c3d1568dc0e4680408de438b2
DafetiteOgaga <bettydafetiteogaga@gmail.com>
2025-12-20 09:07:17 +0100

    spriteAnime.js:

---
+++ estudiebuddie/src/components/animationComps/spriteAnime.js
@@ -0,0 +1,15 @@
+// import {SpriteAnimator} from 'react-sprite-animator';
+// import lionSprite from './coin_1.png';
+
+// function LionDance() {
+//   return (
+//     <SpriteAnimator
+//       sprite={lionSprite}
+//       width={128}
+//       height={128}
+//       frameCount={10}
+//       fps={12}
+//     />
+//   );
+// }
+// export { LionDance };
