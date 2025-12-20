d90415aece100ba00df3c2e38d435fccecb1e943
DafetiteOgaga <bettydafetiteogaga@gmail.com>
2025-12-20 09:07:29 +0100

    banner.js:

---
+++ estudiebuddie/src/components/dynamicPages/banner.js
@@ -0,0 +1,18 @@
+
+
+function Banner() {
+	return (
+		<>
+			{/* <div className="container"> */}
+				<div className="section-title mb-0 pb-2">
+					<h2 className="text-white">Join Our Community Now!</h2>
+					<p className="text-white">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Donec malesuada lorem maximus mauris scelerisque, at rutrum nulla dictum. Ut ac ligula sapien. Suspendisse cursus faucibus finibus.</p>
+				</div>
+				<div className="text-center">
+					<a href="#" className="site-btn">Register Now</a>
+				</div>
+			{/* </div> */}
+		</>
+	)
+}
+export { Banner };
