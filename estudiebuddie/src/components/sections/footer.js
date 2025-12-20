a3756232a7ccf86278a901bdd9eaa0d70cc33eb5
DafetiteOgaga <bettydafetiteogaga@gmail.com>
2025-12-20 09:07:04 +0100

    footer.js:

---
+++ estudiebuddie/src/components/sections/footer.js
@@ -0,0 +1,30 @@
+// import { showPage } from "../../hooks/templatemo-glossy-touch";
+
+function Footer() {
+	return (
+		<div id="footer">
+			<div className="container">
+				<footer className="glass">
+					<div className="footer-content">
+						<div className="footer-links">
+							<a href="##"
+							// onClick={()=>showPage('about')}
+							>About Us</a>
+							<a href="##">Privacy Policy</a>
+							<a href="##">Terms of Service</a>
+							<a href="##">XML Sitemap</a>
+							<a href="##"
+							// onClick={()=>showPage('contact')}
+							>Contact</a>
+						</div>
+						<div className="copyright">
+							&copy; 2025 Glossy Touch. All rights reserved. Crafted with modern web technologies. 
+							Provided by <a rel="noreferrer" href="https://templatemo.com" target="_blank">TemplateMo</a>
+						</div>
+					</div>
+				</footer>
+			</div>
+		</div>
+	)
+}
+export { Footer };
