d7c9e83a59c378de07f7d504b9b644c687c91738
DafetiteOgaga <bettydafetiteogaga@gmail.com>
2026-01-07 13:50:45 +0100

    App.js: removed used imports and codes

--- estudiebuddie/src/App.js
+++ estudiebuddie/src/App.js
@@ -1,23 +1,14 @@
-// import logo from './logo.svg';
 import './App.css';
-// import { Index } from './components';
-// import { Courses } from './components/courses';
-// import { Blog } from './components/blog';
-// import { Contact } from './components/contact';
-// import { Element } from './components/elements';
-// import { SingleCourse } from './components/single-course';
-// import './assets/css/bootstrap.min.css'
-// import './assets/css/style.css'
-// import './assets/css/owl.carousel.css'
-// import './assets/css/font-awesome.min.css'
 import { AppRoutes } from './routes/route';
 import { ToastContainer } from 'react-toastify';
-import { GameResult } from './components/animationComps/confettiAnime';
-import { CoinsRain } from './components/animationComps/framerAnime';
-import { DancingLion, TalkingAnimal } from './components/animationComps/lottieAnime';
-import { LionDance } from './components/animationComps/spriteAnime';
-// import '@fortawesome/fontawesome-free/css/all.min.css';
 import './assets/css/glossy-style.css';
+
+import { library } from "@fortawesome/fontawesome-svg-core";
+import { faEye, faEyeSlash, faCircleCheck, faCheck, faTimes } from "@fortawesome/free-solid-svg-icons";
+library.add(
+  faEye, faEyeSlash, faCircleCheck, faCheck,
+  faTimes
+);
 
 function App() {
   // console.log('App rendered');
@@ -26,7 +17,7 @@ function App() {
       <AppRoutes />
       <ToastContainer
       toastClassName="custom_toast"
-      position={"top-center"} // {deviceType?"top-center":"top-right"}
+      position={"top-right"} // {deviceType?"top-center":"top-right"}
       autoClose={3000} // 3 seconds (you can increase if needed)
       hideProgressBar={false}
       newestOnTop={true}
@@ -36,18 +27,6 @@ function App() {
       draggable
       pauseOnHover
       />
-      {/* <Index /> */}
-      {/* <Courses /> */}
-      {/* <Blog /> */}
-      {/* <Contact /> */}
-      {/* <Element /> */}
-      {/* <SingleCourse /> */}
-
-
-      {/* <GameResult win={true} /> */}
-      {/* <CoinsRain /> */}
-      {/* <DancingLion /> */}
-      {/* <TalkingAnimal talking={true} /> */}
     </>
   );
 }
