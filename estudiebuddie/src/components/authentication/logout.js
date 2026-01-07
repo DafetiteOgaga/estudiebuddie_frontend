07bf5ed3bbaa83d9d4f30cb7c2865dea1620d297
DafetiteOgaga <bettydafetiteogaga@gmail.com>
2026-01-07 14:00:42 +0100

    logout.js:

---
+++ estudiebuddie/src/components/authentication/logout.js
@@ -0,0 +1,21 @@
+import { useCreateStorage } from "../../hooks/persistToStorage";
+import { toast } from "react-toastify"
+import { useAuth } from "../../hooks/authContext";
+
+function useLogout() {
+	console.log('trying to logout')
+	const { setLoggedIn } = useAuth();
+	const { lStorage } = useCreateStorage();
+
+	const logout = () => {
+		console.log('logging out now')
+		lStorage.logout();
+		setLoggedIn(false);
+		console.log('logout success')
+		toast.success("Logout successful");
+	};
+
+	return logout;
+}
+
+export { useLogout };
