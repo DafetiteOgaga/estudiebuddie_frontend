1acf6ba1652b27e43b7bf01fe42e259a45ba40a0
DafetiteOgaga <bettydafetiteogaga@gmail.com>
2026-01-07 14:03:01 +0100

    footer.js: removed unused code

--- estudiebuddie/src/components/sections/footer.js
+++ estudiebuddie/src/components/sections/footer.js
@@ -8,21 +8,8 @@ function Footer() {
 			<div className="container">
 				<footer className="glass">
 					<div className="footer-content">
-						{/* <div className="footer-links">
-							<a href="##"
-							// onClick={()=>showPage('about')}
-							>About Us</a>
-							<a href="##">Privacy Policy</a>
-							<a href="##">Terms of Service</a>
-							<a href="##">XML Sitemap</a>
-							<a href="##"
-							// onClick={()=>showPage('contact')}
-							>Contact</a>
-						</div> */}
 						<div className="copyright">
 							<DafetiteFooter />
-							{/* &copy; 2025 Glossy Touch. All rights reserved. Crafted with modern web technologies. 
-							Provided by <a rel="noreferrer" href="https://templatemo.com" target="_blank">TemplateMo</a> */}
 						</div>
 					</div>
 				</footer>
