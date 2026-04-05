84241c9a1bf9d2c1108d31fab5a8f1be45ad22e3
DafetiteOgaga <bettydafetiteogaga@gmail.com>
2026-04-05 16:46:23 +0100

    authContext.js:

---
+++ estudiebuddie/src/contexts/authContext.js
@@ -0,0 +1,30 @@
+import { createContext, useState, useContext, useEffect, useRef } from "react";
+import { useCreateStorage } from "../hooks/persistToStorage";
+
+// 1. Create the context
+export const AuthContext = createContext();
+
+// 2. Create a provider component
+export function AuthProvider({ children }) {
+	const { lStorage, sStorage } = useCreateStorage()
+	const [loggedIn, setLoggedIn] = useState(false);
+	console.log('AuthContext rendered');
+	useEffect(() => {
+		const isLoggedIn = !!lStorage.getItem('user')?.id
+		setLoggedIn(isLoggedIn)
+	}, [])
+	console.log({loggedIn})
+
+	return (
+		<AuthContext.Provider value={{
+			loggedIn, setLoggedIn
+			}}>
+			{children}
+		</AuthContext.Provider>
+	);
+}
+
+// 4. Create a custom hook for easy access to context
+const useAuth = () => useContext(AuthContext);
+export { useAuth };
+
