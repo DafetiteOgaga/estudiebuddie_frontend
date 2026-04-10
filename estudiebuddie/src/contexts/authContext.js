8b5f5798570424b453def257c5de847b2efd7883
DafetiteOgaga <bettydafetiteogaga@gmail.com>
2026-04-10 05:08:47 +0100

    authContext.js: added logic to handle theme changes

--- estudiebuddie/src/contexts/authContext.js
+++ estudiebuddie/src/contexts/authContext.js
@@ -7,17 +7,46 @@ export const AuthContext = createContext();
 // 2. Create a provider component
 export function AuthProvider({ children }) {
 	const { lStorage, sStorage } = useCreateStorage()
+	// const [reloadBgThemeKey, setReloadBgThemeKey] = useState(0); // trigger
+
+	// const refreshBgTheme = () => {
+	// 	console.log('refresh active')
+	// 	setReloadBgThemeKey(prev => prev + 1);
+	// };
+	const [user, setUser] = useState(() => {
+		return lStorage.getItem("user");
+	});
+	// const theme = user?.theme_mode
+	// console.log({theme})
 	const [loggedIn, setLoggedIn] = useState(false);
 	console.log('AuthContext rendered');
 	useEffect(() => {
-		const isLoggedIn = !!lStorage.getItem('user')?.id
+		const theme = user?.theme_mode
+		console.log({theme})
+		console.log('changing theme')
+		const shouldUseDark = theme==="dark"
+		if (shouldUseDark) {
+			console.log('change to dark...')
+			document.documentElement.classList.add("dark-theme");
+		} else {
+			console.log('change to blue...')
+			document.documentElement.classList.remove("dark-theme");
+		}
+		// document.documentElement.classList.toggle(
+		// 	"dark-theme",
+		// 	theme === "dark"
+		// );
+	}, [user])
+
+	useEffect(() => {
+		const isLoggedIn = !!user?.id
 		setLoggedIn(isLoggedIn)
 	}, [])
 	console.log({loggedIn})
 
 	return (
 		<AuthContext.Provider value={{
-			loggedIn, setLoggedIn
+			loggedIn, setLoggedIn, setUser
 			}}>
 			{children}
 		</AuthContext.Provider>
